aed1210c527a04e4437c3ab30e78dea92f965977
roxana-toma <toma.iroxana@gmail.com>
2024-09-07 18:06:57 +0200

    feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+populateForm();
+
+form.addEventListener('input', throttle(onInput, 500));
+
+form.addEventListener('submit', onSubmit);
+
+function onInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  console.log(formData);
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+}
+
+function populateForm() {
+  if (formData.email) {
+    form.elements.email.value = formData.email;
+  }
+  if (formData.message) {
+    form.elements.message.value = formData.message;
+  }
+}
