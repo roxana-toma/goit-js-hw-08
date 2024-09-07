b1f60280e25cf6dfeb15d9ccdc39a136a6ccb8fd
roxana-toma <toma.iroxana@gmail.com>
2024-09-07 17:57:43 +0200

    v1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,10 +4,8 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-// Selectăm elementul <ul> pentru galeria de imagini
 const galleryList = document.querySelector('.gallery');
 
-// Generăm markup-ul pentru fiecare imagine din array-ul galleryItems
 const galleryMarkup = galleryItems
   .map(({ preview, original, description }) => {
     return `
@@ -24,11 +22,9 @@ const galleryMarkup = galleryItems
   })
   .join('');
 
-// Adăugăm markup-ul în HTML, în elementul <ul>
 galleryList.innerHTML = galleryMarkup;
 
-// Inițializăm SimpleLightbox pentru a face imaginile clickabile și pentru a le deschide în lightbox
 const lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',  // Setăm legenda să fie luată din atributul "alt" al imaginii
-  captionDelay: 250,    // Setăm o întârziere de 250ms pentru afișarea legendei
+  captionsData: 'alt',
+  captionDelay: 250,
 });
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const saveCurrentTime = throttle(function (data) {
+  localStorage.setItem(STORAGE_KEY, data.seconds);
+  console.log(`Current time saved: ${data.seconds} seconds`);
+}, 1000);
+
+player.on('timeupdate', saveCurrentTime);
+
+const savedTime = localStorage.getItem(STORAGE_KEY);
+
+if (savedTime) {
+  player.setCurrentTime(parseFloat(savedTime)).catch(function (error) {
+    console.error('Error current time setup:', error);
+  });
+}
